0b40f175672538d1de2ca46c3d7959b230f8946a
Nirmal Raj <42131933+nirmalraj2000@users.noreply.github.com>
2021-12-26 11:21:41 +0530

    First Commit

---
+++ src/books.js
@@ -0,0 +1,60 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+export class ListData {
+  // var title ;
+  // var description;
+  // var imageUrl;
+
+  constructor(id, title, description, imageUrl) {
+    this.title = title;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.id = id;
+  }
+}
+
+export const bookListObjects = [
+  new ListData(
+    1,
+    "Best Book",
+    `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+    "https://www.rd.com/wp-content/uploads/2019/11/shutterstock_509582812-e1574100998595-scaled.jpg?resize=700,466"
+  ),
+  new ListData(
+    2,
+    "Book of Magic",
+    `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+    "https://thumbs.dreamstime.com/b/old-book-flying-letters-magic-light-background-bookshelf-library-ancient-books-as-symbol-knowledge-history-218640948.jpg"
+  ),
+  new ListData(
+    3,
+    "Book of Knowledge",
+    `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+    "https://media.istockphoto.com/photos/school-picture-id1016131800?k=20&m=1016131800&s=170667a&w=0&h=qEJXSSFd2Peteh8BDvd83gBm8KmVoqtx_YfgDB7F4pE="
+  ),
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./indeex.css";
+import { bookListObjects } from "./books";
+
+export const bookListData = [
+  {
+    imageUrl:
+      "https://www.rd.com/wp-content/uploads/2019/11/shutterstock_509582812-e1574100998595-scaled.jpg?resize=700,466",
+    bookName: "Best Book",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+  },
+  {
+    imageUrl:
+      "https://thumbs.dreamstime.com/b/old-book-flying-letters-magic-light-background-bookshelf-library-ancient-books-as-symbol-knowledge-history-218640948.jpg",
+    bookName: "Book of Magic",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+  },
+  {
+    imageUrl:
+      "https://media.istockphoto.com/photos/school-picture-id1016131800?k=20&m=1016131800&s=170667a&w=0&h=qEJXSSFd2Peteh8BDvd83gBm8KmVoqtx_YfgDB7F4pE=",
+    bookName: "Book of Knowledge",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+  },
+];
+
+function Greeting() {
+  // return <Book></Book>;
+  return <BookList></BookList>;
+}
+
+function BookList() {
+  return (
+    <section className="bookList">
+      {bookListObjects.map((bookData) => {
+        return <Book key={bookData.id} {...bookData}></Book>;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  console.log(props);
+  // const data = props.
+  return (
+    <article
+      className="book"
+      // onMouseOver={() => {
+      //   console.log("Hello World!!!");
+      // }}
+      onMouseEnter={
+        // console.log("");
+        () => {
+          console.log("Mouse Entered");
+        }
+      }
+      onMouseLeave={() => {
+        console.log("Mouse Leave");
+      }}
+      onMouseMove={() => {
+        console.log("Mouse Move");
+      }}
+    >
+      <img
+        // src="https://thumbs.dreamstime.com/b/demo-computer-key-to-download-version-software-trial-64543995.jpg"
+        src={props.imageUrl}
+        alt=""
+      />
+      <h1
+        onClick={() => {
+          alert("Hello Heading.");
+        }}
+      >
+        {props.title}
+      </h1>
+      <h3>{props.description}</h3>
+      <button
+        type="button"
+        onClick={() => {
+          alert("Hello World!");
+        }}
+      >
+        Click Me
+      </button>
+    </article>
+  );
+};
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
