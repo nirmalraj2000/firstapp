b92232081eaa86565e085c058882fd1db12ac93c
Nirmal Raj <42131933+nirmalraj2000@users.noreply.github.com>
2021-12-26 11:53:57 +0530

    Added one more Item

--- src/books.js
+++ src/books.js
@@ -57,4 +57,18 @@ export const bookListObjects = [
           including versions of Lorem Ipsum.`,
     "https://media.istockphoto.com/photos/school-picture-id1016131800?k=20&m=1016131800&s=170667a&w=0&h=qEJXSSFd2Peteh8BDvd83gBm8KmVoqtx_YfgDB7F4pE="
   ),
+  new ListData(
+    4,
+    "Book of Knowledge V2",
+    `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.`,
+    "https://media.istockphoto.com/photos/school-picture-id1016131800?k=20&m=1016131800&s=170667a&w=0&h=qEJXSSFd2Peteh8BDvd83gBm8KmVoqtx_YfgDB7F4pE="
+  ),
 ];
